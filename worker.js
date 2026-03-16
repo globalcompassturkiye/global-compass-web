ad878183eb834b474a8a4895f15820451c51f5d0
globalcompassturkiye <canmuratsubat@gmail.com>
2026-03-17 00:33:42 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,17 @@
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
-    
-    if (url.pathname.startsWith('/api/')) {
+
+    // API isteklerini paytr worker'a yönlendir
+    if (url.pathname.startsWith('/api/paytr')) {
       const newUrl = request.url.replace(
-        'https://www.globalcompass.com.tr/api/',
-        'https://global-compass-paytr.canmuratsubat.workers.dev/'
+        'https://www.globalcompass.com.tr/api/paytr',
+        'https://global-compass-paytr.canmuratsubat.workers.dev'
       );
       return fetch(new Request(newUrl, request));
     }
 
+    // Statik dosyaları serve et
     const response = await env.ASSETS.fetch(request);
     const newResponse = new Response(response.body, response);
     newResponse.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
